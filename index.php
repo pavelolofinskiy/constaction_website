1bd9f1282f1dbe4b43bcdef1b7e44e2f020b9030
pavelolofinskiy <pavelolofinskiy@gmail.com>
2024-05-09 19:24:43 +0300

    initial commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="css\main.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Basic HTML Page</title>
+</head>
+<body>
+
+<header>
+    <div class="header">
+    <a href="#default" class="logo">CompanyLogo</a>
+    <div class="header-right">
+        <a class="active" href="#home">Home</a>
+        <a href="#contact">Contact</a>
+        <a href="#about">About</a>
+    </div>
+    </div> 
+</header>
+
+<main>
+
+</main>
+
+</body>
+</html>
---
+++ page.php
@@ -0,0 +1,19 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css\main.css">
+</head>
+</html>
+
+
+
+<?php
+
+function olofinskiy_name_reciver(){
+}
+
+olofinskiy_name_reciver()
+
+?>
