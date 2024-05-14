1edd4627427db5fd1052a87b6edf6c23ba742d53
pavelolofinskiy <pavelolofinskiy@gmail.com>
2024-05-14 12:04:54 +0300

    added contact page

--- contact.php
+++ contact.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <link rel="stylesheet" href="css\reset.css">
-    <link rel="stylesheet" href="css\main.css">
+    <link rel="stylesheet" href="css\contact.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>My Basic HTML Page</title>
@@ -11,15 +11,15 @@
 
 <header class="my-header">
     <nav class="left_spacing">
-        <a class="active" href="#home">Home</a>
-        <a href="contact.php">Contact</a>
+        <a class="active" href="index.php">Home</a>
+        <a href="#contact">Contact</a>
         <a href="#about">About</a>
     </nav>
 </header>
 
 <main>
     <div class="my-div">
-        <p>Пажилая</p>
+        <p>Контакт</p>
     </div>
 </main>
 
