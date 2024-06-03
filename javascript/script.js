494d2032022d20b31e0112c8636d2c831b8c5d2c
pavelolofinskiy <pavelolofinskiy@gmail.com>
2024-06-03 18:50:58 +0300

    image fix

---
+++ javascript/script.js
@@ -0,0 +1,9 @@
+var div = document.getElementById('script-div');
+
+function ChangeDivStyle () {
+        div.style.paddingBottom = '200px';
+        div.style.backgroundColor = 'white'
+        div.addEventListener
+}
+
+ChangeDivStyle()
